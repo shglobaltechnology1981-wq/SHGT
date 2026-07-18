e50d62853bfa10e175361c925f06576ac83b763f
SHGT <shglobaltechnology1981@gmail.com>
2026-07-18 11:02:06 +0600

    Add alert for script confirmation and fetch products

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+alert("Script Working");
+
+fetch("products.json")
+.then(response => response.json())
+.then(products => {
+    ...
+});
 fetch("products.json")
   .then(res => res.json())
   .then(data => {
