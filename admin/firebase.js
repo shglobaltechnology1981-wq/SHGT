d0851f5f9683ab53432ac372bddfcfd4677dd2af
SHGT <shglobaltechnology1981@gmail.com>
2026-07-27 19:41:05 +0600

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -57,3 +57,15 @@ location.href="login.html";
 });
 
 }
+// firebase.js
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
