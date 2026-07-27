79e1d5b02d4537d3378fb5bafa37eba3c4f6cc2b
SHGT <shglobaltechnology1981@gmail.com>
2026-07-27 23:03:17 +0600

    Upgrade Firebase SDK to version 10.12.5
    Updated Firebase SDK version and modified configuration settings.

--- admin/firebase.js
+++ admin/firebase.js
@@ -1,74 +1,21 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-
-import {
-getAuth,
-signInWithEmailAndPassword,
-signOut
-} from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-firestore.js";
+import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.5/firebase-storage.js";
 
 const firebaseConfig = {
-
-apiKey: "YOUR_API_KEY",
-
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-
-projectId: "YOUR_PROJECT_ID",
-
-storageBucket: "YOUR_PROJECT.appspot.com",
-
-messagingSenderId: "YOUR_SENDER_ID",
-
-appId: "YOUR_APP_ID"
-
+  apiKey: "AIzaSyBfsyRl1h-egJ6cq8fEB-6sWndX20KNsD4",
+  authDomain: "shgt-d629e.firebaseapp.com",
+  projectId: "shgt-d629e",
+  storageBucket: "shgt-d629e.firebasestorage.app",
+  messagingSenderId: "884656945032",
+  appId: "1:884656945032:web:0a5d2788213388c2a36c3d"
 };
 
 const app = initializeApp(firebaseConfig);
 
 const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
-window.login = function(){
-
-const email=document.getElementById("email").value;
-
-const password=document.getElementById("password").value;
-
-signInWithEmailAndPassword(auth,email,password)
-
-.then(()=>{
-
-location.href="dashboard.html";
-
-})
-
-.catch(error=>{
-
-alert(error.message);
-
-});
-
-}
-
-window.logout=function(){
-
-signOut(auth).then(()=>{
-
-location.href="login.html";
-
-});
-
-}
-// firebase.js
-
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-auth.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-storage.js";
-
-const app = initializeApp(firebaseConfig);
-
-export const auth = getAuth(app);
-export const db = getFirestore(app);
-export const storage = getStorage(app);
-<script type="module" src="admin.js"></script>
-</body>
-</html>
+export { auth, db, storage };
